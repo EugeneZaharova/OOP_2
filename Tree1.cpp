b8d7f509b90c42c0a52fbdd248c6511e6e6fd023
EugeneZaharova <eugenezaharova@mail.ru>
2016-10-13 20:33:30 +0400

    Update Tree1.cpp

--- Tree1.cpp
+++ Tree1.cpp
@@ -40,15 +40,6 @@ void Tree::push(Node& elem) {
 		elem.children = NULL;
 		++count;
 	}
-	/*TreeItem *elem = new TreeItem(trian);
-	root = elem;
-
-	if (trian > root) {
-		push(trian->right);
-	}
-	else {
-		push(trian->left);
-	}*/
 }
 
 bool Tree::empty() {
@@ -63,15 +54,6 @@ Triangle Tree::pop(Node& elem) {
 		return elem.trian;
 		delete[] elem.children;
 	}
-	/*Triangle result;
-	if (root != nullptr) {
-		TreeItem *old_root = root;
-		root = root->GetNext();
-		result = old_root->GetTriangle();
-		old_root->SetNext(nullptr);
-		delete old_root;
-	}
-	return result;*/
 }
 
 Tree::~Tree() {
