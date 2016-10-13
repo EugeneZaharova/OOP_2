674d398615a9fb4ea8ad24d5f3061a4be30c4435
EugeneZaharova <eugenezaharova@mail.ru>
2016-10-13 20:32:03 +0400

    Update Tree1.cpp

--- Tree1.cpp
+++ Tree1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 Tree::Tree() : root(nullptr) {
 }
 
-Tree::Tree(const Tree& orig) {
+Tree::Tree(Tree& orig) {
 	root = orig.root;
 }
 
@@ -19,26 +19,26 @@ std::ostream& operator<<(std::ostream& os, const Tree& tree) {
 	return os;
 }
 
-void Tree::push(Node* elem) {
-	elem->number = n++;
+void Tree::push(Node& elem) {
+	elem.number = ++n;
 	cout << "Enter the data: " << endl;
-	cin >> elem->trian;
+	std::istream&operator>>(std::istream&is, Node elem);
 
 	cout << "Enter the amount of children: " << endl;
 	unsigned int quantity;
 	cin >> quantity;
 
 	if (quantity > 0) {
-		elem->childCount = quantity;
-		elem->children = new Node[quantity];
-		for (unsigned int amount = 0; amount < quantity; amount++) {
-			Tree::push(&elem->children[amount]);
+		elem.childCount = quantity;
+		elem.children = new Node[quantity];
+		for (unsigned int amount = 0; amount < quantity; ++amount) {
+			Tree::push(elem.children[amount]);
 		}
 	}
 	else {
-		elem->childCount = 0;
-		elem->children = NULL;
-		count++;
+		elem.childCount = 0;
+		elem.children = NULL;
+		++count;
 	}
 	/*TreeItem *elem = new TreeItem(trian);
 	root = elem;
@@ -55,13 +55,13 @@ bool Tree::empty() {
 	return root == nullptr;
 }
 
-Triangle Tree::pop(Node* elem) {
-	if (elem->childCount > 0) {
-		for (unsigned int amount = 0; amount < elem->childCount; amount++) {
-			Tree::pop(&elem->children[amount]);
+Triangle Tree::pop(Node& elem) {
+	if (elem.childCount > 0) {
+		for (unsigned int amount = 0; amount < elem.childCount; ++amount) {
+			Tree::pop(elem.children[amount]);
 		}
-		return elem->trian;
-		delete[] elem->children;
+		return elem.trian;
+		delete[] elem.children;
 	}
 	/*Triangle result;
 	if (root != nullptr) {
